108a2d4d7069bb3d1c9bf76d5008fa489cf26151
Tyler <creepinator45@gmail.com>
2023-02-18 15:56:35 -0800

    Created basic gameplay loop (Incomplete) Created PiecePlacement and Coordinate structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::num::ParseIntError;
+
 #[derive(Copy, Clone, PartialEq, Debug)]
 enum Size {
     Small,
@@ -31,6 +33,18 @@ struct GameState {
 }
 
 #[derive(Debug)]
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug)]
+struct PiecePlacement {
+    coordinate: Coordinate,
+    size: Size,
+}
+
+#[derive(Debug)]
 enum MatchDir {
     Horizontal,
     Vertical,
@@ -38,16 +52,49 @@ enum MatchDir {
     RightDiagonal,
 }
 
+impl PiecePlacement {
+    fn ask_player() -> Result<PiecePlacement, ParseIntError> {
+        let mut position_input = (String::new(), String::new());
+    
+        println!("Position: X");
+        std::io::stdin()
+            .read_line(&mut position_input.0)
+            .expect("Failed to read line");
+    
+        println!("Position: Y");
+        std::io::stdin()
+            .read_line(&mut position_input.1)
+            .expect("Failed to read line");
+    
+        let mut size_input = String::new();
+    
+        println!("Size:");
+        std::io::stdin()
+            .read_line(&mut size_input)
+            .expect("Failed to read line");
+    
+        let move_coordinate = Coordinate {
+            x: position_input.0.trim().parse()?,
+            y: position_input.0.trim().parse()?,
+        };
+    
+        Result::Ok(PiecePlacement {
+            coordinate: move_coordinate,
+            size: Size::Small,
+        })
+    }
+}
+
 impl GameState {
-    fn check_cell(&self, coordinate: (usize, usize)) -> Result<Vec<MatchDir>, &'static str> {
-        if coordinate.0 > 7 || coordinate.0 < 2 {
+    fn check_cell(&self, coordinate: Coordinate) -> Result<Vec<MatchDir>, &'static str> {
+        if coordinate.x > 7 || coordinate.x < 2 {
             return Result::Err("X Coordinate Out of Bounds");
         }
-        if coordinate.1 > 7 || coordinate.0 < 2 {
+        if coordinate.y > 7 || coordinate.x < 2 {
             return Result::Err("Y Coordinate Out of Bounds");
         }
 
-        match &self.game_board[coordinate.0][coordinate.1] {
+        match &self.game_board[coordinate.x][coordinate.y] {
             Cell::OutOfBounds => Result::Err("Placing in Null Cell"),
             Cell::Empty => Result::Ok(Vec::new()),
             Cell::Piece(Piece {
@@ -60,12 +107,12 @@ impl GameState {
                 if let Cell::Piece(Piece {
                     owner: current_owner1,
                     size: _,
-                }) = self.game_board[coordinate.0 + 1][coordinate.1]
+                }) = self.game_board[coordinate.x + 1][coordinate.y]
                 {
                     if let Cell::Piece(Piece {
                         owner: current_owner2,
                         size: _,
-                    }) = self.game_board[coordinate.0 - 1][coordinate.1]
+                    }) = self.game_board[coordinate.x - 1][coordinate.y]
                     {
                         if current_owner == &current_owner1 && &current_owner1 == &current_owner2 {
                             matches.push(MatchDir::Horizontal);
@@ -76,12 +123,12 @@ impl GameState {
                 if let Cell::Piece(Piece {
                     owner: current_owner1,
                     size: _,
-                }) = self.game_board[coordinate.0][coordinate.1 + 1]
+                }) = self.game_board[coordinate.x][coordinate.y + 1]
                 {
                     if let Cell::Piece(Piece {
                         owner: current_owner2,
                         size: _,
-                    }) = self.game_board[coordinate.0][coordinate.1 - 1]
+                    }) = self.game_board[coordinate.x][coordinate.y - 1]
                     {
                         if current_owner == &current_owner1 && &current_owner1 == &current_owner2 {
                             matches.push(MatchDir::Vertical);
@@ -92,12 +139,12 @@ impl GameState {
                 if let Cell::Piece(Piece {
                     owner: current_owner1,
                     size: _,
-                }) = self.game_board[coordinate.0 + 1][coordinate.1 + 1]
+                }) = self.game_board[coordinate.x + 1][coordinate.y + 1]
                 {
                     if let Cell::Piece(Piece {
                         owner: current_owner2,
                         size: _,
-                    }) = self.game_board[coordinate.0 - 1][coordinate.1 - 1]
+                    }) = self.game_board[coordinate.x - 1][coordinate.y - 1]
                     {
                         if current_owner == &current_owner1 && &current_owner1 == &current_owner2 {
                             matches.push(MatchDir::LeftDiagonal);
@@ -108,12 +155,12 @@ impl GameState {
                 if let Cell::Piece(Piece {
                     owner: current_owner1,
                     size: _,
-                }) = self.game_board[coordinate.0 + 1][coordinate.1 - 1]
+                }) = self.game_board[coordinate.x + 1][coordinate.y - 1]
                 {
                     if let Cell::Piece(Piece {
                         owner: current_owner2,
                         size: _,
-                    }) = self.game_board[coordinate.0 - 1][coordinate.1 + 1]
+                    }) = self.game_board[coordinate.x - 1][coordinate.y + 1]
                     {
                         if current_owner == &current_owner1 && &current_owner1 == &current_owner2 {
                             matches.push(MatchDir::RightDiagonal);
@@ -128,29 +175,37 @@ impl GameState {
     fn check_board(&self) {
         for row_index in 2..7 {
             for column_index in 2..7 {
-                let _ = dbg!(&self.check_cell((row_index, column_index)).unwrap());
+                let _ = &self
+                    .check_cell(Coordinate {
+                        x: row_index,
+                        y: column_index
+                    })
+                    .unwrap();
             }
         }
     }
 
-    fn place_piece(&mut self, coordinate: (usize, usize), size: Size) -> Result<(), &'static str> {
-        let player_index = dbg!(self.turn_count % self.turn_order.len());
+    fn place_piece(&mut self, piece_placement: PiecePlacement) -> Result<(), &'static str> {
+        let player_index = self.turn_count % self.turn_order.len();
 
-        let coordinate = (coordinate.0 + 2, coordinate.1 + 2);
+        let coordinate = Coordinate {
+            x: piece_placement.coordinate.x + 2,
+            y: piece_placement.coordinate.y + 2,
+        };
 
-        if coordinate.0 > 7 || coordinate.0 < 2 {
+        if coordinate.x > 7 || coordinate.x < 2 {
             return Result::Err("X Coordinate Out of Bounds");
         }
-        if coordinate.1 > 7 || coordinate.0 < 2 {
+        if coordinate.y > 7 || coordinate.x < 2 {
             return Result::Err("Y Coordinate Out of Bounds");
         }
 
-        if self.game_board[coordinate.0][coordinate.1] != Cell::Empty {
+        if self.game_board[coordinate.x][coordinate.y] != Cell::Empty {
             return Result::Err("Placement Position Occupied");
         }
         if !self.turn_order[player_index].piece_pool.contains(&Piece {
             owner: player_index,
-            size,
+            size: piece_placement.size,
         }) {
             return Result::Err("Requested Piece Size Not Available");
         }
@@ -162,15 +217,15 @@ impl GameState {
                 .position(|x| {
                     *x == Piece {
                         owner: player_index,
-                        size,
+                        size: piece_placement.size,
                     }
                 })
                 .expect("needle not found"),
         );
 
-        self.game_board[coordinate.0][coordinate.1] = Cell::Piece(Piece {
+        self.game_board[coordinate.x][coordinate.y] = Cell::Piece(Piece {
             owner: (player_index),
-            size: (size),
+            size: (piece_placement.size),
         });
 
         let dirs = [
@@ -184,17 +239,20 @@ impl GameState {
             (1, 0),
         ];
         for d in dirs {
-            if let Cell::Piece(_) = self.game_board[coordinate.0 - 1 + d.0][coordinate.1 - 1 + d.1] {
-                match self.game_board[coordinate.0 - 2 + d.0 * 2][coordinate.1 - 2 + d.1 * 2] {
+            if let Cell::Piece(_) = self.game_board[coordinate.x - 1 + d.0][coordinate.y - 1 + d.1]
+            {
+                match self.game_board[coordinate.x - 2 + d.0 * 2][coordinate.y - 2 + d.1 * 2] {
                     Cell::Piece(_) => continue,
                     Cell::OutOfBounds => {
-                        self.game_board[coordinate.0 - 1 + d.0][coordinate.1 - 1 + d.1] = Cell::Empty;
+                        self.game_board[coordinate.x - 1 + d.0][coordinate.y - 1 + d.1] =
+                            Cell::Empty;
                     }
                     Cell::Empty => {
-                        self.game_board[coordinate.0 - 2 + d.0 * 2][coordinate.1 - 2 + d.1 * 2] =
-                            self.game_board[coordinate.0 - 1 + d.0][coordinate.1 - 1 + d.1].clone();
-    
-                        self.game_board[coordinate.0 - 1 + d.0][coordinate.1 - 1 + d.1] = Cell::Empty;
+                        self.game_board[coordinate.x - 2 + d.0 * 2][coordinate.y - 2 + d.1 * 2] =
+                            self.game_board[coordinate.x - 1 + d.0][coordinate.y - 1 + d.1].clone();
+
+                        self.game_board[coordinate.x - 1 + d.0][coordinate.y - 1 + d.1] =
+                            Cell::Empty;
                     }
                 }
             }
@@ -206,200 +264,42 @@ impl GameState {
 fn init() -> GameState {
     GameState {
         game_board: [
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::Empty,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
-            [
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-                Cell::OutOfBounds,
-            ],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::Empty,       Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds,],
+            [ Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds, Cell::OutOfBounds,],
         ],
         turn_order: [
             Player {
                 name: String::from("Player 1"),
                 piece_pool: vec![
-                    Piece {
-                        owner: 0,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 0,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 0,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 0,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 0,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 0,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 0,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 0,
-                        size: Size::Small,
-                    },
+                    Piece { owner: 0, size: Size::Small,},
+                    Piece { owner: 0, size: Size::Small,},
+                    Piece { owner: 0, size: Size::Small,},
+                    Piece { owner: 0, size: Size::Small,},
+                    Piece { owner: 0, size: Size::Small,},
+                    Piece { owner: 0, size: Size::Small,},
+                    Piece { owner: 0, size: Size::Small,},
+                    Piece { owner: 0, size: Size::Small,},
                 ],
             },
             Player {
                 name: String::from("Player 2"),
                 piece_pool: vec![
-                    Piece {
-                        owner: 1,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 1,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 1,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 1,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 1,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 1,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 1,
-                        size: Size::Small,
-                    },
-                    Piece {
-                        owner: 1,
-                        size: Size::Small,
-                    },
+                    Piece { owner: 1, size: Size::Small,},
+                    Piece { owner: 1, size: Size::Small,},
+                    Piece { owner: 1, size: Size::Small,},
+                    Piece { owner: 1, size: Size::Small,},
+                    Piece { owner: 1, size: Size::Small,},
+                    Piece { owner: 1, size: Size::Small,},
+                    Piece { owner: 1, size: Size::Small,},
+                    Piece { owner: 1, size: Size::Small,},
                 ],
             },
         ],
@@ -411,20 +311,12 @@ fn main() {
     let mut game_state = init();
 
     loop {
-        let mut position_input = String::new();
+        let player_move = PiecePlacement::ask_player().unwrap();
 
-        println!("Position:");
-        std::io::stdin()
-            .read_line(&mut position_input)
-            .expect("Failed to read line");
+        //println!("You will attempt to place make move {:?}", player_move);
 
-        let mut size_input = String::new();
+        game_state.place_piece(player_move).unwrap();
 
-        println!("Size:");
-        std::io::stdin()
-            .read_line(&mut size_input)
-            .expect("Failed to read line");
-
-        println!("You will attempt to place a {size_input} piece at {position_input}");
+        game_state.check_board()
     }
 }
